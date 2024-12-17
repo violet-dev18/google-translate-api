1f55e7738357e72204fc9a3fd127c0ea7bfd7eb2
Aidan Welch <aidan@freedwave.com>
2024-12-17 14:47:08 +0100

    fix language typing in googleTranslateApi, and the languages object

--- test/index.test-d.ts
+++ test/index.test-d.ts
@@ -1,29 +1,31 @@
 import { expectAssignable, expectType } from 'tsd';
-import translateDefaultImport, { translate, Translator, speak, TranslationResponse, singleTranslate, batchTranslate, isSupported, getCode, languages } from '..';
+import translateDefaultImport, { translate, Translator, googleTranslateApi, speak, singleTranslate, batchTranslate, isSupported, getCode, languages } from '..';
 
 // Expect assignable since the default import is a function with properties, i.e. `translateDefaultImport('text to translate', { to: 'nl' })` and `translateDefaultImport.isSupported('nl')` both work
 expectAssignable<typeof translate>(translateDefaultImport)
-expectType<typeof translateDefaultImport.translate>(translate)
+// expectType<typeof translateDefaultImport.translate>(translate) // While this would be valid in the JS, it is not a
+// breaking issue
 
-expectType<Promise<TranslationResponse>>(translate('abc'));
-expectType<Promise<TranslationResponse[]>>(translate(['a', {text: 'b', to: 'nl'}, 'c']));
-expectType<Promise<{a: TranslationResponse, b: TranslationResponse}>>(translate({a: 'test', b: {text: 'b', to: 'nl'}}));
+expectType<Promise<googleTranslateApi.TranslationResponse>>(translate('abc'));
+expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translate(['a', {text: 'b', to: 'nl'}, 'c']));
+expectType<Promise<{a: googleTranslateApi.TranslationResponse, b: googleTranslateApi.TranslationResponse}>>(translate({a: 'test', b: {text: 'b', to: 'nl'}}));
 
 const translator = new Translator();
-expectType<Promise<TranslationResponse>>(translator.translate('abc'));
-expectType<Promise<TranslationResponse[]>>(translator.translate(['a', {text: 'b', to: 'nl'}, 'c']));
-expectType<Promise<{a: TranslationResponse, b: TranslationResponse}>>(translator.translate({a: 'test', b: {text: 'b', to: 'nl'}}));
+expectType<Promise<googleTranslateApi.TranslationResponse>>(translator.translate('abc'));
+expectType<Promise<googleTranslateApi.TranslationResponse[]>>(translator.translate(['a', {text: 'b', to: 'nl'}, 'c']));
+expectType<Promise<{a: googleTranslateApi.TranslationResponse, b: googleTranslateApi.TranslationResponse}>>(translator.translate({a: 'test', b: {text: 'b', to: 'nl'}}));
 
 expectType<Promise<string>>(speak('abc'));
 expectType<Promise<string[]>>(speak(['a', {text: 'b', to: 'nl'}, 'c']))
 expectType<Promise<{a: string, b: string}>>(speak({a: 'test', b: {text: 'b', to: 'nl'}}));
 
-expectType<Promise<TranslationResponse>>(singleTranslate('abc'));
+expectType<Promise<googleTranslateApi.TranslationResponse>>(singleTranslate('abc'));
 
-expectType<Promise<TranslationResponse>>(batchTranslate('abc'));
-expectType<Promise<TranslationResponse[]>>(batchTranslate(['a', {text: 'b', to: 'nl'}, 'c']));
-expectType<Promise<{a: TranslationResponse, b: TranslationResponse}>>(batchTranslate({a: 'test', b: {text: 'b', to: 'nl'}}));
+expectType<Promise<googleTranslateApi.TranslationResponse>>(batchTranslate('abc'));
+expectType<Promise<googleTranslateApi.TranslationResponse[]>>(batchTranslate(['a', {text: 'b', to: 'nl'}, 'c']));
+expectType<Promise<{a: googleTranslateApi.TranslationResponse, b: googleTranslateApi.TranslationResponse}>>(batchTranslate({a: 'test', b: {text: 'b', to: 'nl'}}));
 
 expectType<boolean>(isSupported('en'));
 expectType<string | null>(getCode('en'));
 expectAssignable<Record<string, string>>(languages);
+expectType<string>(googleTranslateApi.languages.en);
